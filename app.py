a789e5d4c1a500812cd33f992ac0de09a79fa77f
Boris <bor.prilyan@gmail.com>
2026-04-16 09:32:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ with col2:
     lobby_interior = st.number_input("Lobby Interior (m2)", min_value=0.0, step=1.0)
 
 with col3:
-    st.subheader("External and Infrastructure")
+    st.subheader("External & Infrastructure")
     rooftop = st.number_input("Rooftop (m2)", min_value=0.0, step=1.0)
     facilities = st.number_input("Facilities (m2)", min_value=0.0, step=1.0)
     landscape = st.number_input("External/Landscape (m2)", min_value=0.0, step=1.0)
